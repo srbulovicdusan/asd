ce069c38760f238a175dd72917cfb3341efaeaff
srbulovicdusan <srbulovicdusan@gmail.com>
2020-02-08 11:20:00 +0100

    message

--- asd/src/asd/Main.java
+++ asd/src/asd/Main.java
@@ -2,6 +2,15 @@ package asd;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("asd");
+		System.out.println("asddd");
+		System.out.println("asddd");
+
+		System.out.println("asddd");
+		System.out.println("asddd");
+
+		System.out.println("asddd");
+
+		System.out.println("asddd");
+
 	}
 }
